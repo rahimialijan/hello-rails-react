dbcbfad963b8fd91a351775a828ee37f798edf06
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-09-26 22:31:10 +0500

    setup the reducer

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,19 @@
-import React from "react";
-import ReactDOM from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMessage } from "../slices/greetingSlice";
 
 function Greeting() {
-  return <h1>Greeting project</h1>;
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greeting.greeting);
+  const status = useSelector((state) => state.greeting.status);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchMessage());
+    }
+  }, [status, dispatch]);
+
+  return <div>{status === "succeeded" ? greeting : "Cargando..."}</div>;
 }
 
 export default Greeting;
